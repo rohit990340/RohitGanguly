d1a602fcd009dfc017cd6fc0694e64ee4602e897
Rohit Ganguly <tanushriganguly7@gmail.com>
2024-11-28 16:41:36 +0530

    test commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.get("/date", (req, res) => {
+    res.send(new Date().toString());
+});
+
+app.use((req, res) => {
+    res.status(404).send("Page not found");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
